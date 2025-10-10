94973e272f32649a9e3a27a47a78e173d91ad409
Marina <marina.slepinina3@gmail.com>
2025-10-10 19:13:43 +0300

    added sprite, Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+import css from "./Header.module.css";
+
 export const Header = () => {
-    return <p>Header</p>;
+    return (
+        <header className={css.header}>
+            <nav className={css.nav}>
+                <Link href="/" className={css.navLogo}>
+                    <svg width={136} height={16}>
+                        <use href="/sprite.svg#icon-TravelTrucks"></use>
+                    </svg>
+                </Link>
+                <ul className={css.navList}>
+                    <li className={css.navItem}>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className={css.navItem}>
+                        <Link href="/catalog">Catalog</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
